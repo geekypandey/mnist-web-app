bd32727785d256f4db8eecbdfe2c99cbcab0ac32
bruhh <anuragcarbon1998@gmail.com>
2020-01-08 10:49:10 +0530

    multiprocessing included with some hack

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 import pathlib
 import time
+import multiprocessing
 
 from flask import Flask,render_template,request,flash,redirect
 from flask_bootstrap import Bootstrap 
@@ -17,7 +18,6 @@ path = pathlib.Path('models/')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'secret_key'
-
 bootstrap = Bootstrap(app)
 
 class PhotoForm(FlaskForm):
@@ -30,24 +30,31 @@ def init():
     global learn
     print('Loaded')
     learn = load_learner(path,'mnist.pkl')
+    global pool
+    pool = multiprocessing.Pool(2)
 
+def get_pred(file):
+    img = open_image(file)
+    pred_class,_,_ = learn.predict(img)
+    return pred_class
 
 @app.route('/',methods=['POST','GET'])
 def index():
     form = PhotoForm()
+    global output 
+    global pool
     output = {}
     if form.validate_on_submit():
         uploaded_files = request.files.getlist("photo")
         for file in uploaded_files:
-            if file:
-                filename = secure_filename(file.filename)
-                img_src = os.path.join('static/images',filename)
-                file.save(img_src)
-                image = open_image(img_src)
-                pred_class,pred_idx,outputs = learn.predict(image)
-                output[file.filename] = pred_class
-        return render_template('output.html',output=output)
+            file.save(f'static/images/{file.filename}')
+        uploaded_files = [f'static/images/{file.filename}' for file in uploaded_files]
+        predictions = pool.map(get_pred,uploaded_files)
+        return render_template('output.html',output=predictions)
+
     return render_template('index.html',form=form)  
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
